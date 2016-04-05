d9dedbdf556edfb214443d3d6a4fd95cc04efde3
Jaymi <jaj14b@my.fsu.edu>
2016-04-05 10:44:28 -0400

    Project, database, and GitHub setup

--- app/femr/ui/controllers/ApplicationController.java
+++ app/femr/ui/controllers/ApplicationController.java
@@ -2,7 +2,7 @@ package femr.ui.controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-
+//Application controller
 public class ApplicationController extends Controller {
     public static Result removeTrailingSlash(String path) {
         return movedPermanently("/" + path);
